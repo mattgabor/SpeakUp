936bd1987b5052cb218b0a63c0a36cd06343c717
Matt Gabor <mattgabor@gmail.com>
2015-04-04 15:32:39 -0700

    feature #1 Complete!!!

--- app.js
+++ app.js
@@ -12,18 +12,31 @@ var app = require('express')(),
   bluemix = require('./config/bluemix'),
   watson = require('watson-developer-cloud'),
   extend = require('util')._extend;
+  app.set('view engine', 'jade');
+
 // if bluemix credentials exists, then override local
 var credentials = extend({
   version:'v1',
 	username: '<username>',
 	password: '<password>'
 }, bluemix.getServiceCreds('speech_to_text')); // VCAP_SERVICES
+
+app.set('views', __dirname + '/views'); //optional since express defaults to CWD/views
+
+// render index page
+app.get('/', function(req, res){
+	res.render('index');
+});
+
 // Create the service wrapper
 var speechToText = watson.speech_to_text(credentials);
+
 // Configure express
 require('./config/express')(app, speechToText);
+
 // Configure sockets
 require('./config/socket')(io, speechToText);
+
 var port = process.env.VCAP_APP_PORT || 3000;
 server.listen(port);
 console.log('listening at:', port);
