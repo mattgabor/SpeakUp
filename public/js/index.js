8e1c768fd3f4eaef72fb0d0d7a439256fcaec46c
Corey Ching <cycching@ucdavis.edu>
2015-04-04 21:43:40 -0700

    matt

--- public/js/index.js
+++ public/js/index.js
@@ -5,12 +5,12 @@ console.log("hello2'");
 function dataTransfer () {
 
     var data = document.getElementById("text");
+    changePage();
+    console.log(data);
+
+}
+
+function changePage( callback) {
     window.location='/results';
-
-    var temp = data.innerHTML;
-
-    console.log(temp);
-
-
-
+    callback();
 }
