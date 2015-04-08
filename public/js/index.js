3cb949a84d883b61038985c60b4c85785e663ab4
Corey Ching <cycching@ucdavis.edu>
2015-04-07 20:15:52 -0700

    updated results page for refreshing

--- public/js/index.js
+++ public/js/index.js
@@ -2,32 +2,32 @@
  * Created by coreyching on 4/4/15.
  */
 
-function changePage( callback) {
-    window.location='/results';
-    callback();
-}
+$(Document).ready(function() {
 
-function goHome( callback) {
-    window.location='/';
-    callback();
-}
+    function goHome(callback) {
+        window.location = '/';
+        callback();
+    }
 
+    function analysis() {
 
-function analysis() {
+        document.getElementById("analyze").click(); // Click on the checkbox
 
-    document.getElementById("analyze").click(); // Click on the checkbox
+    }
 
-}
+    $('#abcd').click(function () {
 
-$('#abcd').click(function() {
+        $.ajax({
+            url: '/results',
+            type: 'POST',
+            data: {'results': $('#text').text()}, // An object with the key 'submit' and value 'true;
+            error: function (req, err) {
+                console.log('my message' + err);
+            }
+        });
+        window.location.replace('/results');
+        location.reload();
+        analysis();
 
-    $.ajax({
-        url: '/results',
-        type: 'POST',
-        data: {'results':  $('#text').text()}, // An object with the key 'submit' and value 'true;
-        error: function(req, err){ console.log('my message' + err); }
     });
-    window.location.replace('/results');
-    analysis();
-
 });
